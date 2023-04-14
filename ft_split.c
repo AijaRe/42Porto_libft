cc844732427231c9804adb2e4de6f0b509b0d5f8
Aija Repsa <120313261+AijaRe@users.noreply.github.com>
2023-04-14 19:06:15 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -14,5 +14,42 @@
 
 char	**ft_split(char const *s, char c)
 {
+	char	**new;
+	int	i;
+	int	x;
+	int	y;
+	
+	i = 0;
+	x = 0;
+	y = 0;
+	while (s[i] != '\0')
+	{
+		if(s[i] == c)
+		{
+			new[x][y] = '\0';
+			x++;
+			y = 0;
+		}
+		else
+		{
+			new[x][y] = s[i];
+			y++;
+		}
+		i++;
+	}
+	return (new);		
+}
 
+int	main(void)
+{
+	char	**result;
+	int	i = 0;
+	
+	char *test = "test of string";
+	result = ft_split(test, ' ');
+	while (result[i] != '\0')
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
 }
