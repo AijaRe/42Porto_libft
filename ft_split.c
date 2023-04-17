7ca8ffad6e83ad523389ad5d2bd19338561a90bc
Aija Repsa <120313261+AijaRe@users.noreply.github.com>
2023-04-17 18:55:45 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -12,31 +12,105 @@
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+size_t	ft_strlen(const char *s)
 {
-	char	**new;
-	int	i;
-	int	x;
-	int	y;
-	
+	size_t	i;
+
 	i = 0;
-	x = 0;
-	y = 0;
 	while (s[i] != '\0')
 	{
-		if(s[i] == c)
+		i++;
+	}
+	return (i);
+}
+/*return an array of strings obtained by splitting ’s’*/ 
+/*use the character ’c’ as a delimiter*/
+/*array must end with a NULL pointer*/
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+	size_t	i;
+	
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (res == 0)
+		return (0);
+	i = 0;
+	while (s[start+i] != '\0' && i < len)
+	{
+		res[i] = s[start+i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);	
+}
+static int	count_strs(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	strs;
+	
+	i = 0;
+	j = 0;
+	strs = 0;
+	
+	while (s[i] != '\0')
+	{
+		while (s[i + j] != '\0' && s[i + j] != c)
+			j++;
+		if (j > 0)
 		{
-			new[x][y] = '\0';
-			x++;
-			y = 0;
+			i = i + j;
+			j = 0;
+			strs++;
 		}
 		else
+			i++;
+	}
+	return (strs);
+}
+static void	sep_strs(char const *s, char c, char **new)
+{
+	int	i;
+	int	j;
+	int	a;
+	
+	i = 0;
+	j = 0;
+	a = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
 		{
-			new[x][y] = s[i];
-			y++;
+			new[a] = ft_substr(s, j, i - j);
+			{
+			a++;
+			j = i + 1;
+			}
 		}
 		i++;
 	}
+	if (j < i)
+	{
+		new[a] = ft_substr(s, j, i - j);
+		a++;
+	}
+}
+char	**ft_split(char const *s, char c)
+{
+	char	**new;
+	int	str_nb;
+	int	i;
+	int	j;
+	int	a;
+	
+	str_nb = count_strs(s, c);
+	new = malloc(sizeof(char *) * (str_nb + 1));
+	if (!new)
+		return NULL;
+	sep_strs(s, c, new);
+	new[str_nb + 1] = NULL;
 	return (new);		
 }
 
@@ -45,11 +119,15 @@ int	main(void)
 	char	**result;
 	int	i = 0;
 	
-	char *test = "test of string";
+	char *test = "test the string";
 	result = ft_split(test, ' ');
-	while (result[i] != '\0')
+	while (result[i] != 0)
 	{
 		printf("%s\n", result[i]);
+		free(result[i]);
 		i++;
 	}
+	free(result);
+	return (0);
 }
+
