702212821e103f5e99837e91bd6418673d144621
Aija Repsa <120313261+AijaRe@users.noreply.github.com>
2023-04-27 17:15:07 +0100

    Delete mains directory

--- mains/main_strncmp.c
+++
@@ -1,3 +0,0 @@
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-
-
--- mains/main_strtrim.c
+++
@@ -1,6 +0,0 @@
-char	*ft_strtrim(char const *s1, char const *set);
-
-int	main(void)
-{
-	printf("%s\n", ft_strtrim("12332abcde21", "231"));
-}
